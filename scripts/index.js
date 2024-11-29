0f7948576d9d493d2a4c7e62ada8dcaf8855a725
insxmnea <kostya.svetashov.01@gmail.com>
2024-11-29 13:01:12 +0400

    feat: update styles

--- scripts/index.js
+++ scripts/index.js
@@ -1,40 +1,60 @@
 const output = document.querySelector("#speech");
 const recordButton = document.querySelector("#record");
+const continuousCheckbox = document.querySelector("#continuous");
+const interimCheckbox = document.querySelector("#interim");
+
 const res = [];
 
 const speechRec = new p5.SpeechRec("ru-RU", gotSpeech);
-let continuous = true;
+let continuous = false;
 let interimResults = false;
 
 let isRecording = false;
 
 function setup() {
   noCanvas();
-
-  // speechRec.start(continuous, interimResults);
 }
 
 function gotSpeech() {
-  console.log(speechRec);
   if (speechRec.resultValue) {
-    let said = speechRec.resultString;
+    res.push(`#${res.length + 1}: ${speechRec.resultString}`);
 
-    res.push(said);
+    output.innerHTML = res.join("\n");
 
-    output.innerHTML = res.join("<br>");
+    output.scrollTop = output.scrollHeight;
   }
+
+  stopRecording();
+}
+
+function startRecording() {
+  speechRec.start(continuous, interimResults);
+  isRecording = true;
+  recordButton.classList.remove("not-recording");
+  recordButton.classList.add("recording");
+}
+
+function stopRecording() {
+  speechRec.stop();
+  isRecording = false;
+  recordButton.classList.remove("recording");
+  recordButton.classList.add("not-recording");
 }
 
 recordButton.addEventListener("click", () => {
   if (isRecording) {
-    speechRec.stop();
-    isRecording = false;
-    recordButton.classList.remove("recording");
-    recordButton.classList.add("not-recording");
+    stopRecording();
   } else {
-    speechRec.start(continuous, interimResults);
-    isRecording = true;
-    recordButton.classList.remove("not-recording");
-    recordButton.classList.add("recording");
+    startRecording();
   }
+});
+
+continuousCheckbox.addEventListener("change", () => {
+  stopRecording();
+  continuous = continuousCheckbox.checked;
+});
+
+interimCheckbox.addEventListener("change", () => {
+  stopRecording();
+  interimResults = interimCheckbox.checked;
 });
